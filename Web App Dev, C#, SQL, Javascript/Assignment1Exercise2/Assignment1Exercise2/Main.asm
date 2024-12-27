cb2ce289b49782da9088b38c25d640f735730d2a
michaellank2049 <102771415+michaelank2049@users.noreply.github.com>
2024-12-27 13:47:24 -0600

    Add files via upload

---
+++ Web App Dev, C#, SQL, Javascript/Assignment1Exercise2/Assignment1Exercise2/Main.asm
@@ -0,0 +1,24 @@
+INCLUDE Irvine32.inc
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+
+.data
+	Monday    = 1
+	Tuesday   = 2
+	Wednesday = 3
+	Thursday  = 4
+	Friday    = 5
+	Saturday  = 6
+	Sunday    = 7
+
+	DayOfTheWeekArray BYTE Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+
+.code
+main proc			
+
+	invoke ExitProcess,0
+main endp
+end main
